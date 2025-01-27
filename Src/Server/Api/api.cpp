dfa5507ebf715cafbd061961f3d2c39747b8aa79
Evan UHRING <146000775+Evan-UHRING@users.noreply.github.com>
2025-01-27 09:17:43 +0100

    animation when to long to wait a request + we can now terminate the server :D (windows and mac compatible)

--- Src/Server/Api/api.cpp
+++ Src/Server/Api/api.cpp
@@ -7,6 +7,7 @@
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <windows.h>
 #else
 #include <unistd.h>
 #include <sys/socket.h>
--- Src/Server/Api/server.cpp
+++ Src/Server/Api/server.cpp
@@ -1,19 +1,33 @@
 #include "api.cpp"
 
 void disableInput() {
-    termios tty;
-    tcgetattr(STDIN_FILENO, &tty); // Get current terminal settings
-    tty.c_lflag &= ~ICANON;        // Disable canonical mode
-    tty.c_lflag &= ~ECHO;          // Disable echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty); // Apply changes
+    #ifdef _WIN32
+        HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+        DWORD prevMode;
+        GetConsoleMode(hInput, &prevMode);          // Get current input mode
+        SetConsoleMode(hInput, prevMode & ~ENABLE_ECHO_INPUT & ~ENABLE_LINE_INPUT); // Disable echo and line input
+    #else
+        termios tty;
+        tcgetattr(STDIN_FILENO, &tty); // Get current terminal settings
+        tty.c_lflag &= ~ICANON;        // Disable canonical mode
+        tty.c_lflag &= ~ECHO;          // Disable echo
+        tcsetattr(STDIN_FILENO, TCSANOW, &tty); // Apply changes
+    #endif
 }
 
 void enableInput() {
-    termios tty;
-    tcgetattr(STDIN_FILENO, &tty); // Get current terminal settings
-    tty.c_lflag |= ICANON;         // Re-enable canonical mode
-    tty.c_lflag |= ECHO;           // Re-enable echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty); // Apply changes
+    #ifdef _WIN32
+        HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+        DWORD prevMode;
+        GetConsoleMode(hInput, &prevMode);          // Get current input mode
+        SetConsoleMode(hInput, prevMode | ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT); // Re-enable echo and line input
+    #else
+        termios tty;
+        tcgetattr(STDIN_FILENO, &tty); // Get current terminal settings
+        tty.c_lflag |= ICANON;         // Re-enable canonical mode
+        tty.c_lflag |= ECHO;           // Re-enable echo
+        tcsetattr(STDIN_FILENO, TCSANOW, &tty); // Apply changes
+    #endif
 }
 
 int main() {
