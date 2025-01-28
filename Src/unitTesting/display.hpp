1e32b59f8fec8a016f828e57f9f6ecca60c2b602
aurelienfernandez <aurelien.fernandez@algosup.com>
2025-01-28 12:02:36 +0100

    Chore & feat: Added functions & comments
    Addition of comments and multiple functions.
    Added functions:
    - Functions managing prints, terminal clearing & error handling,
    - Function to test dijkstra's algorithm,
    - Creation of a UI to navigate through the different tests,
    - Function to duplicate csv files into the Src folder for tests.

---
+++ Src/unitTesting/display.hpp
@@ -0,0 +1,99 @@
+#ifndef DISPLAY
+#define DISPLAY
+#include "unitTesting.hpp"
+
+/**
+ * This function prints all nodes from a defined path defined as a vector of ints.
+ * @param vec: a vector of ints
+ * @result Prints a list of ints with this format [1, 2, 3].
+ */
+void printVector(const vector<int> &vec)
+{
+    cout << "[";
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        cout << vec[i];
+        if (i != vec.size() - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << "]\n";
+}
+
+/**
+ * Clear the terminal.
+ * @param None
+ * @result The terminal is cleared.
+ */
+void clearScreen()
+{
+#if defined _WIN32
+    system("cls");
+#elif defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+#elif defined(__APPLE__)
+    system("clear");
+#endif
+}
+
+/**
+ * This function handles exceptions related to user inputs.
+ * @param None
+ * @result If the user entered an incorrect input, prints an error message.
+ */
+void handleException()
+{
+    if (invalid)
+    {
+        cout << "Invalid input!\n";
+        invalid = false;
+    }
+}
+
+/**
+ * This function requests the selection of an correct or an incorrect csv file.
+ * @param None
+ * @result Copy the file selected by the user into the "Src folder".
+ */
+void csvSelection()
+{
+    while (true)
+    {
+
+        clearScreen();
+        cout << "+---------------------------------------------------------------+\n";
+        cout << "|                    Which file do you wish to use?             |\n";
+        cout << "++---------------------------------------------+----------------+\n";
+        cout << "1| A clean CSV file                            |\n";
+        cout << "-+---------------------------------------------+\n";
+        cout << "2| A CSV file with loops, repetitions, etc...  |\n";
+        cout << "-+---------------------------------------------+\n";
+        cout << "0| Cancel                                      |\n";
+        cout << "-+---------------------------------------------+\n";
+
+        handleException();
+        cout << "\n";
+        cin >> input;
+
+        if (input == "0")
+        {
+            return;
+        }
+        else if (input == "1")
+        {
+            copyFile("correct.csv");
+            return;
+        }
+        else if (input == "2")
+        {
+            copyFile("incorrect.csv");
+            return;
+        }
+        else
+        {
+            invalid = true;
+        }
+    }
+}
+#endif // !DISPLAY
--- Src/unitTesting/unitTesting.cpp
+++ Src/unitTesting/unitTesting.cpp
@@ -1,96 +1,52 @@
-#include <assert.h>
-#include <list>
-#include "../Server/Algorithm/algorithm.cpp"
-
-void printVector(const std::vector<int> &vec)
-{
-    std::cout << "[";
-    for (size_t i = 0; i < vec.size(); ++i)
-    {
-        std::cout << vec[i];
-        if (i != vec.size() - 1)
-        {
-            std::cout << ", ";
-        }
-    }
-    std::cout << "]\n";
-}
+#include "unitTesting.hpp"
+#include "dijsktraTests.hpp"
+#include "display.hpp"
 
 int main()
 {
-    // Declare and initialize testPaths
-    list<pair<int, int>> testPaths = {
-        {4, 1},  // Test 1
-        {9, 3},  // Test 2
-        {2, 10}, // Test 3
-        {7, 5},  // Test 4
-        {6, 8},  // Test 5
-        {5, 10}, // Test 6
-        {3, 2},  // Test 7
-        {8, 8},  // Test 8
-        {9, 6},  // Test 9
-        {3, 7},  // Test 10
-        {1, 8},  // Test 11
-        {6, 1},  // Test 12
-        {10, 8}, // Test 13
-        {9, 8}   // Test 14
-    };
-
-    // Declare and initialize results
-    list<tuple<double, vector<int>>> results = {
-        {11, vector<int>{4, 6, 3, 1}},  // Test 1
-        {12, vector<int>{9, 8, 3}},     // Test 2
-        {23, vector<int>{2, 4, 10}},    // Test 3
-        {12, vector<int>{7, 8, 5}},     // Test 4
-        {13, vector<int>{6, 3, 8}},     // Test 5
-        {26, vector<int>{5, 4, 10}},    // Test 6
-        {9, vector<int>{3, 2}},         // Test 7
-        {0, vector<int>{8}},            // Test 8
-        {14, vector<int>{9, 8, 3, 6}},  // Test 9
-        {14, vector<int>{3, 6, 7}},     // Test 10
-        {17, vector<int>{1, 3, 8}},     // Test 11
-        {8, vector<int>{6, 3, 1}},      // Test 12
-        {28, vector<int>{10, 4, 2, 8}}, // Test 13
-        {1, vector<int>{9, 8}}          // Test 14
-    };
-    graph = loadDataset();
-
-    cout << "Testing Dijkstra's algorithm:\n";
-
-    auto pathIt = testPaths.begin();
-    auto resultIt = results.begin();
-
-    vector<int> path; // Resulting path of the algorithm
-    double path_time; // Resulting time
-    int testID = 1;
-    // Iterate through testPaths and results simultaneously
-    for (; pathIt != testPaths.end() && resultIt != results.end(); ++pathIt, ++resultIt)
+    while (true)
     {
-        const auto &[start, end] = *pathIt;
+        clearScreen();
+        cout << "+---------------------------------------------------------------+\n";
+        cout << "| Welcome to PathQuick's unit tests, what do you want to test?  |\n";
+        cout << "++-------------------+------------------------------------------+\n";
+        cout << "1| Pathing algorithm |\n";
+        cout << "-+-------------------+\n";
+        cout << "2| Server functions  |\n";
+        cout << "-+-------------------+\n";
+        cout << "3| Data validation   |\n";
+        cout << "-+-------------------+\n";
+        cout << "0| Quit              |\n";
+        cout << "-+-------------------+\n";
 
-        path = modifiedDijkstra(graph, start, end, &path_time);
-        if (make_tuple(path_time, path) == *resultIt)
+        handleException(); // If the previous input is invalid, print an error message
+
+        cout << "\n";
+        cin >> input;
+        if (input == "0")
         {
-            cout << "\e[0;32mTest " << testID << " passed\e[0;37m\n";
+            clearScreen();
+            return 0;
+        }
+        else if (input == "1")
+        {
+            clearScreen();
+            copyFile("./correct.csv");
+
+            dijkstraTesting();
+        }
+        else if (input == "2")
+        {
+            clearScreen();
+        }
+        else if (input == "3")
+        {
+            clearScreen();
+            csvSelection();
         }
         else
         {
-            cout << "\e[0;31m===========================Test " << testID << "===========================\n";
-            cout << "Expected Value:\n"
-                 << "Time: " << get<0>(*resultIt)
-                 << "\n"
-                 << "Path: ";
-            printVector(get<1>(*resultIt));
-            cout << "\nOutput Value:\n"
-                 << "Time: " << path_time
-                 << "\n"
-                 << "Path: ";
-            printVector(path);
-            cout << "============================================================\n";
-
-            cout << "\e[0;37m";
+            invalid = true;
         }
-        testID++;
     }
-    return 0;
 }
