67181588343d566e6ccd702ec237091b568c6b11
aurelienfernandez <aurelien.fernandez@algosup.com>
2025-01-30 13:40:12 +0100

    feat: switch from gnu 17 to 20

--- Src/Client/startClient.cmd
+++ Src/Client/startClient.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-g++ -std=c++17 main.cpp ../Libraries/Tinyxml2/tinyxml2.cpp -o Bin\pathQuick.exe
-.\Bin\pathQuick.exe
+g++ -std=c++17 -o Bin\pathQuick.exe  main.cpp ../Libraries/Tinyxml2/tinyxml2.cpp -lws2_32
+Bin\pathQuick.exe
 pause
--- Src/Server/startServer.cmd
+++ Src/Server/startServer.cmd
@@ -1,3 +1,5 @@
 @echo off
-g++ -std=gnu++20 -O3 -o Bin/server.exe -IInclude -Llib -lws2_32 Api/server.cpp ../Libraries/Tinyxml2/tinyxml2.cpp
+g++ -std=gnu++20 -O3 -o Bin/server.exe Api/server.cpp ../Libraries/Tinyxml2/tinyxml2.cpp -IInclude -Llib -lws2_32
+cls
 Bin\server.exe
+pause
