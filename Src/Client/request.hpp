5b9ef880d3a99cf1e74377c1d3e65ddad2bac989
aurelienfernandez <aurelien.fernandez@algosup.com>
2025-01-30 11:22:58 +0100

    Fix: client can now build & run on windows

--- Src/Client/request.hpp
+++ Src/Client/request.hpp
@@ -11,21 +11,27 @@ bool server_is_online = false;
 
 void sendRequest(string http_request, string &body);
 
-void closeSocketRequest(int socket) {
-    #ifdef _WIN32
-        closesocket(socket); // Use Winsock's closesocket on Windows
-    #else
-        close(socket);       // Use POSIX's close on Linux/macOS
-    #endif
+void closeSocketRequest(int socket)
+{
+#ifdef _WIN32
+    closesocket(socket); // Use Winsock's closesocket on Windows
+#else
+    close(socket); // Use POSIX's close on Linux/macOS
+#endif
 }
 
 // URL encoding function
-string urlEncode(const string &str) {
+string urlEncode(const string &str)
+{
     ostringstream encoded;
-    for (unsigned char c : str) {
-        if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
+    for (unsigned char c : str)
+    {
+        if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
+        {
             encoded << c;
-        } else {
+        }
+        else
+        {
             encoded << '%' << setw(2) << setfill('0') << hex << (int)c;
         }
     }
@@ -33,8 +39,10 @@ string urlEncode(const string &str) {
 }
 
 // Function to send the request and handle the response
-void sendRequestQuickPath(int start, int end, string file_format) {
-    cout << "Calculating shortest path..." << endl << endl;
+void sendRequestQuickPath(int start, int end, string file_format)
+{
+    cout << "Calculating shortest path..." << endl
+         << endl;
     auto time_start = chrono::high_resolution_clock::now(); // get time
 
     // Prepare the API request
@@ -49,26 +57,31 @@ void sendRequestQuickPath(int start, int end, string file_format) {
 
     // Build the HTTP GET request
     string http_request = "GET /route?source=" + encoded_source + "&destination=" + encoded_destination + "&format=" + encoded_format + " HTTP/1.1\r\n"
-                          "Host: localhost:8080\r\n"
-                          "Connection: close\r\n"
-                          "\r\n";
+                                                                                                                                        "Host: localhost:8080\r\n"
+                                                                                                                                        "Connection: close\r\n"
+                                                                                                                                        "\r\n";
 
     string body;
     sendRequest(http_request, body);
 
-    if (server_is_online) {
+    if (server_is_online)
+    {
         // Write the body to a file
         string filename = "Bin/pathQuick." + format;
         ofstream file(filename, ios::out);
-        if (!file) {
+        if (!file)
+        {
             cerr << "Error opening file for writing!" << endl;
             return;
         }
-        if (format == "json") {
+        if (format == "json")
+        {
             json json_obj = json::parse(body);
             file << setw(4) << json_obj.dump(4);
             ;
-        } else {
+        }
+        else
+        {
             file << body;
         }
         file.close();
@@ -78,20 +91,25 @@ void sendRequestQuickPath(int start, int end, string file_format) {
         // Measure time
         auto stop = chrono::high_resolution_clock::now();
         auto duration = chrono::duration_cast<chrono::milliseconds>(stop - time_start);
-        if (duration.count() > 2000) {
+        if (duration.count() > 2000)
+        {
             cout << endl
                  << "Path calculated in " << duration.count() / 1000 << " seconds." << endl;
-        } else {
+        }
+        else
+        {
             cout << endl
                  << "Path calculated in " << duration.count() << " milliseconds." << endl;
         }
-    } 
+    }
     else
         cerr << "Error connecting to server!" << endl;
 }
 
-void sendRequestId(int &lowest_id, int &highest_id) {
-    while (true) {
+void sendRequestId(int &lowest_id, int &highest_id)
+{
+    while (true)
+    {
         // Build the HTTP GET request
         string http_request = "GET /id? HTTP/1.1\r\n"
                               "Host: localhost:8080\r\n"
@@ -111,7 +129,8 @@ void sendRequestId(int &lowest_id, int &highest_id) {
             istringstream query_stream(body);
             string param;
 
-            while (getline(query_stream, param, '&')) {
+            while (getline(query_stream, param, '&'))
+            {
                 size_t eq_pos = param.find('=');
                 if (eq_pos == string::npos)
                     continue;
@@ -119,10 +138,12 @@ void sendRequestId(int &lowest_id, int &highest_id) {
                 string key = param.substr(0, eq_pos);
                 string value = param.substr(eq_pos + 1);
 
-                if (key == "min_id") {
+                if (key == "min_id")
+                {
                     lowest_id = stoi(value);
                 }
-                else if (key == "max_id") {
+                else if (key == "max_id")
+                {
                     highest_id = stoi(value);
                 }
             }
@@ -130,11 +151,22 @@ void sendRequestId(int &lowest_id, int &highest_id) {
     }
 }
 
-void sendRequest(string http_request, string &body) {
+void sendRequest(string http_request, string &body)
+{
+    #ifdef _WIN32
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        // Handle Winsock initialization error
+        return;
+    }
+    #endif
     // Set up the socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
+
         cerr << "Error creating socket!" << endl;
         return;
     }
@@ -159,13 +191,14 @@ void sendRequest(string http_request, string &body) {
     // Receive the response
     string response;
     char buffer[4096];
-    #ifdef _WIN32
-        SSIZE_T bytes_received;
-    #else
-        ssize_t bytes_received;
-    #endif
+#ifdef _WIN32
+    SSIZE_T bytes_received;
+#else
+    ssize_t bytes_received;
+#endif
 
-    while ((bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0)) > 0) {
+    while ((bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0)) > 0)
+    {
         buffer[bytes_received] = '\0';
         response += buffer;
     }
@@ -174,7 +207,8 @@ void sendRequest(string http_request, string &body) {
 
     // Separate the HTTP headers and body
     size_t header_end = response.find("\r\n\r\n");
-    if (header_end == string::npos) {
+    if (header_end == string::npos)
+    {
         return;
     }
 
